1e75019f493a6762c6706cffe9835358e3abe289
HamletDRC <hamletdrc@gmail.com>
2010-04-14 14:10:54 +0200

    Grasp - Finished converting all tests to Spock.

--- src/test/groovy/com/canoo/grasp/PresentationModelTest.groovy
+++ src/test/groovy/com/canoo/grasp/PresentationModelTest.groovy
@@ -1,53 +1,70 @@
 package com.canoo.grasp
 
+import com.canoo.grasp.demo.BookPM
+import spock.lang.Specification
 
-class PresentationModelTest extends GroovyTestCase {
+class PresentationModelTest extends Specification {
 
     static final int value = 1
     OneSimpleAttributePM simplePM
     Map simpleModel
 
-    void setUp(){
+    void setup(){
         simpleModel = [attribute: value]
         simplePM = new OneSimpleAttributePM(model:simpleModel)
     }
 
-    void testSetModelLeavesDefaultPropertiesAlone() {
-        println '''A PresentationModel that has no attributes can take a null model
-        effectively showing that PM default props are ignored since an NPE would be thrown otherwise.'''
-        def noAttributePM = new PresentationModel()
-        noAttributePM.model = null
+    void '''A PresentationModel that has no attributes can take a null model
+        effectively showing that PM default props are ignored since an NPE would be thrown otherwise.'''() {
+
+        when:
+            def noAttributePM = new PresentationModel()
+            noAttributePM.model = null
+        then: 
+            notThrown(NullPointerException)
     }
 
-    void testSimplePresentationModel() {
-        assert simplePM.attribute in Attribute
-        assert simplePM.attribute.value == value
+    def "a simple presentation model has an attribute value"() {
+
+        expect:
+            simplePM.attribute in Attribute
+            simplePM.attribute.value == value
     }
 
-    void testPMThatHasAPresentationModelSwitchAttribute() {
-        println '''PresentationModels may have attributes that are of type PresentationModelSwitch
-        thus referring to some other PresentationModel, e.g. a BookPM to a PublisherPM.'''
-        def referringPM = new PMSwitchAttributePM()
-        def initialPM = referringPM.presentationModelReference
-        assert initialPM, 'attributes of type PresentatioModelSwitch must always have a prototype value'
-        assert initialPM.adaptee, 'the adaptee must always be set since it is used for prototyping'
+    def '''PresentationModel attributes of type "Switch" must always have a prototype value and an adaptee, even
+        when a default value is not supplied and it '''() {
 
-        // Now we set the model, which means the given simpleModel must be wrapped in a OneSimpleAttributePM,
-        // which is then set as new adaptee of the switch
-        referringPM.model = [presentationModelReference: simpleModel ]
+        when:
+            def model = new PMSwitchAttributePM()
 
-        assert referringPM.presentationModelReference.is(initialPM), 'the switch instance remains'
+        then:
+            assert model.presentationModelReference
+            assert model.presentationModelReference.adaptee
+    }
 
-        def newPM = referringPM.presentationModelReference.adaptee
-        assert ! newPM.is(simplePM), 'we have a new PM instance'
+    def '''PresentationModels may have attributes that are of type PresentationModelSwitch
+        thus referring to some other PresentationModel, e.g. a BookPM to a PublisherPM.'''() {
 
-        assert newPM in OneSimpleAttributePM
-        assert newPM.attribute in Attribute
-        assert ! newPM.attribute.is(simplePM.attribute) // remember for binding: we have new Attribute instances!
+        when:
+            def referringPM = new PMSwitchAttributePM()
+            def initialPM = referringPM.presentationModelReference
 
-        assert referringPM.presentationModelReference.attribute.value == value
+            // Now we set the model, which means the given simpleModel must be wrapped in a OneSimpleAttributePM,
+            // which is then set as new adaptee of the switch
+            referringPM.model = [presentationModelReference: simpleModel ]
 
-        // todo dk: in the course of this setting, which listeners have been notified?
+            assert referringPM.presentationModelReference.is(initialPM), 'the switch instance remains'
+    
+            def newPM = referringPM.presentationModelReference.adaptee
+
+        then:
+            assert ! newPM.is(simplePM), 'we have a new PM instance'
+            newPM in OneSimpleAttributePM
+            newPM.attribute in Attribute
+            ! newPM.attribute.is(simplePM.attribute) // remember for binding: we have new Attribute instances!
+            referringPM.presentationModelReference.attribute.value == value
+
+            // todo dk: in the course of this setting, which listeners have been notified?
 
     }
 

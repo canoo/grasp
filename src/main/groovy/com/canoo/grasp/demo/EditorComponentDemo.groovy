d4a824b257a782da368acaae2278d3d69b91afe5
Johannes Bühler <johannes.buehler@canoo.com>
2010-04-22 15:01:59 +0200

    bind date picker and date text field

--- src/main/groovy/com/canoo/grasp/demo/EditorComponentDemo.groovy
+++ src/main/groovy/com/canoo/grasp/demo/EditorComponentDemo.groovy
@@ -9,6 +9,7 @@ import com.canoo.grasp.GraspContext
 import org.jdesktop.swingx.JXDatePicker
 import javax.swing.SwingConstants
 import java.awt.event.ActionListener
+import java.text.DateFormat
 
 Book gina = new Book(title: "gina", isbn: "0123456789", author: null, publishDate: new Date(), publisher: new Publisher(name: "publisher"))
 BookPM bookPM = new BookPM(model: gina)
@@ -16,12 +17,16 @@ BookPM bookPM = new BookPM(model: gina)
 GraspContext.useBinding()
 
 SwingBuilder builder = new SwingBuilder()
+def dateFormat = DateFormat.getDateInstance(DateFormat.LONG, Locale.ENGLISH)
 def frame = builder.frame(defaultCloseOperation: WindowConstants.EXIT_ON_CLOSE, title: "How to use an editor component") {
     panel {
         vbox {
             hbox() {
                 label "Date: "
-                textField(columns: 10).bind bookPM.publishDate
+                textField(columns: 10).bind (bookPM.publishDate,
+                        read:{dateFormat.format(it)},
+                        write:{dateFormat.parse(it)},on:"keyReleased")
+
             }
             hbox {
                 label "Locale: "
